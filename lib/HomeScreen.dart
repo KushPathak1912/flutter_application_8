6c3f13aed73d648e6d8ed8ff9013eed8951e0172
KushPathak1912 <kushpathak1912@gmail.com>
2021-09-01 11:22:28 +0530

    xyz

---
+++ lib/HomeScreen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen Future<entends> StatelessWidget;  async {
+@override
+Widget build(BuildContext context){
+    return Scaffold(
+        appBar: AppBar(title: text("Home Screen),
+        ),
+        body: Center(
+            child: TextButton(
+            
+            
+             
+             onPressed: () => logOut(context),
+              child: Text("LogOut))),
+    ),
+    ),
+);
+}
+}
+
+class entends {
+}
